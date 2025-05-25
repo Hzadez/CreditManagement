5305bd5b5ec868c3d2f207ec6977d2b7d7ef7948
Zeynal <hzadez63@gmail.com>
2025-05-25 19:44:08 +0400

    Yeniilik

--- CreditControls/obj/Debug/net8.0/CreditControls.AssemblyInfo.cs
+++ CreditControls/obj/Debug/net8.0/CreditControls.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreditControls")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a0fc5acf1cb7355a584051feced9ca245d5b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreditControls")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreditControls")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
